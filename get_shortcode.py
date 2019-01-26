77355a53e3803eee1b53e0af540f3e1e08d9fe7f
Matthew Fennell <mf2017@ic.ac.uk>
2019-01-26 15:18:31 +0000

    Added function to give us the shortocde of the given language

---
+++ get_shortcode.py
@@ -0,0 +1,8 @@
+import csv
+def get_shortcode_of(language):
+	with open('languages.csv', mode='r') as csvFile:
+		reader = csv.reader(csvFile)
+		for row in reader:
+			if language == row[0]:
+				print(row[1])
+				return row[1]
