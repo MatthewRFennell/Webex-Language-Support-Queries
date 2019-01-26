2e190913fcff13ebe7a9f84352fc5c02525273f6
Matthew Fennell <mf2017@ic.ac.uk>
2019-01-26 15:46:27 +0000

    Added fuzzy matcher

--- get_shortcode.py
+++ get_shortcode.py
@@ -1,8 +1,18 @@
 import csv
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import operator
+
+# Returns true if the find was an exact match, false if it tried to find the neares
 def get_shortcode_of(language):
 	with open('languages.csv', mode='r') as csvFile:
 		reader = csv.reader(csvFile)
+		fuzzy = {}
 		for row in reader:
+			fuzzy[row[0]] = fuzz.ratio(row[0], language)
 			if language == row[0]:
 				print(row[1])
-				return row[1]
+				return true
+		# At this point we haven't found an exact match
+		print(max(fuzzy.items(), key=operator.itemgetter(1))[0])
+		return false
